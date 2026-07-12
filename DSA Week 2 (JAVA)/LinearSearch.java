0a8c172448adaf5c6dd3eb53b28bbe5f8bde012d
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-12 10:49:51 +0530

    Refactor LinearSearch.java for cleaner code

--- DSA Week 2 (JAVA)/LinearSearch.java
+++ DSA Week 2 (JAVA)/LinearSearch.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
-
 public class LinearSearch {
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 30, 40, 50};
-
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter Element: ");
             int key = sc.nextInt();
-
             boolean found = false;
 
             for(int i = 0; i < arr.length; i++) {
-
                 if(arr[i] == key) {
                     System.out.println("Found at Index " + i);
                     found = true;
                     break;
                 }
             }
-
             if(!found)
                 System.out.println("Element Not Found");
         }
