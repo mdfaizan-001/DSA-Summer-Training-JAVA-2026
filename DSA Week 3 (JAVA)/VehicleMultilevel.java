d59e05eeff7dc3481729de86b89d41d3c43bb296
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-13 10:14:02 +0530

    Refactor VehicleMultilevel.java with consistent formatting

--- DSA Week 3 (JAVA)/VehicleMultilevel.java
+++ DSA Week 3 (JAVA)/VehicleMultilevel.java
@@ -3,21 +3,18 @@ class Vehicle {
         System.out.println("This is a Vehicle");
     }
 }
-
 class FourWheeler extends Vehicle {
     FourWheeler() {
         super(); // Calls the Vehicle constructor
         System.out.println("This is a FourWheeler Vehicle");
     }
 }
-
 class Car extends FourWheeler {
     Car() {
         super(); // calls the FourWheeler constructor
         System.out.println("This 4 Wheeler Vehicle is a Aston Martin DB5");
     }
 }
-
 public class VehicleMultilevel {
     public static void main(String[] args) {
         Car obj = new Car(); // Triggers all the constructors in the order 
