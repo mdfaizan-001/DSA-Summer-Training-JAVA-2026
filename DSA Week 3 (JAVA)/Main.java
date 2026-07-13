aa8048c4eeadd1879982704d5d7d0305038762cf
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-13 10:15:08 +0530

    Update Main.java

--- DSA Week 3 (JAVA)/Main.java
+++ DSA Week 3 (JAVA)/Main.java
@@ -1,13 +1,10 @@
 class Student {
     String name;
-
     // constructor
     Student(String n) {
         this.name = n;
-
     }
 }
-
 public class Main {
     public static void main(String[] args) {
         Student s = new Student("Faizan");
