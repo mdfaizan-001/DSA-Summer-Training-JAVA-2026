2a874a8972d6d31ff7b1f50f76e2eb076e00c0c5
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-05 21:28:47 +0530

    Fix formatting issues in ArraySum.java

--- DSA Week 1 (JAVA)/ArraySum.java
+++ DSA Week 1 (JAVA)/ArraySum.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
-
 public class ArraySum {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int[] arr = new int[5];
             int sum = 0;
-            
             System.out.println("Enter 5 numbers:");
-            
             for(int i = 0; i < 5; i++) {
                 arr[i] = sc.nextInt();
             }
-            
             for(int i = 0; i < 5; i++) {
                 sum += arr[i];
             }
-            
             System.out.println("Sum of all elements = " + sum);
         }
     }
