f6aa864c8c4f74fddbc6287f34519eb0e299cf32
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-05 21:32:53 +0530

    Update PositiveNegativeZero.java

--- DSA Week 1 (JAVA)/PositiveNegativeZero.java
+++ DSA Week 1 (JAVA)/PositiveNegativeZero.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
-
 public class PositiveNegativeZero {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int num;
-            
             System.out.print("Enter a number: ");
             num = sc.nextInt();
-            
             if(num > 0)
                 System.out.println("Positive Number");
             else if(num < 0)
