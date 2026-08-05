4208883d35468bfd063e003065767425a4017de2
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-05 21:31:25 +0530

    Refactor EvenOddChecker.java formatting

--- DSA Week 1 (JAVA)/EvenOddChecker.java
+++ DSA Week 1 (JAVA)/EvenOddChecker.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
-
 public class EvenOddChecker {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int num;
-            
             System.out.print("Enter a number: ");
             num = sc.nextInt();
-            
             if (num % 2 == 0)
                 System.out.println(num + " is Even");
             else
