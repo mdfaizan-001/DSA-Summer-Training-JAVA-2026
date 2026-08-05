3336452b033de8ef651b6da45e01e9ad881aab2a
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-05 21:27:30 +0530

    Update ArraySearch.java

--- DSA Week 1 (JAVA)/ArraySearch.java
+++ DSA Week 1 (JAVA)/ArraySearch.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
-
 public class ArraySearch {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int[] arr = new int[5];
             int search;
             boolean found = false;
-            
             System.out.println("Enter 5 numbers:");
-            
             for(int i = 0; i < 5; i++) {
                 arr[i] = sc.nextInt();
             }
-            
             System.out.print("Enter element to search: ");
             search = sc.nextInt();
-            
             for(int i = 0; i < 5; i++) {
                 if(arr[i] == search) {
                     found = true;
                     break;
                 }
             }
-            
             if(found)
                 System.out.println("Element Found");
             else
