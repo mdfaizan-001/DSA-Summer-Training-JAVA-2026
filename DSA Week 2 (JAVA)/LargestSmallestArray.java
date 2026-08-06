182a1b1d24a44c3c70855c01f2df5055f1a8694f
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-06 19:39:41 +0530

    Update LargestSmallestArray.java

--- DSA Week 2 (JAVA)/LargestSmallestArray.java
+++ DSA Week 2 (JAVA)/LargestSmallestArray.java
@@ -1,10 +1,8 @@
 public class LargestSmallestArray {
     public static void main(String[] args) {
         int[] arr = { 5, 2, 9, 1, 7 };
-
         int largest = arr[0];
         int smallest = arr[0];
-        
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > largest) 
                 largest = arr[i];
