3cd648b5985996a22563cdadaecbaaf88b77fcd1
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-06 19:45:09 +0530

    Update ExceptionHandling.java

--- DSA Week 3 (JAVA)/ExceptionHandling.java
+++ DSA Week 3 (JAVA)/ExceptionHandling.java
@@ -1,27 +1,19 @@
 public class ExceptionHandling {
-
     public static void main(String[] args) {
-
         // Example 1: ArithmeticException
         System.out.println("Example 1: Division by Zero");
-
         int n = 10;
         int m = 0;
-
         try {
             int ans = n / m;
             System.out.println("Answer: " + ans);
         } catch (ArithmeticException e) {
             System.out.println("Error: Division by 0!");
         }
-
         System.out.println("-----------------------------");
-
         // Example 2: ArrayIndexOutOfBoundsException with finally
         System.out.println("Example 2: Array Exception");
-
         int[] numbers = {1, 2, 3};
-
         try {
             System.out.println(numbers[5]);
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -29,7 +21,6 @@ public class ExceptionHandling {
         } finally {
             System.out.println("This block always executes.");
         }
-
         System.out.println("Program continues...");
     }
 }
