ed84a06fabf98bc26df7581566dee5866c2a2cea
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-06 19:42:28 +0530

    Update SumAverageArray.java

--- DSA Week 2 (JAVA)/SumAverageArray.java
+++ DSA Week 2 (JAVA)/SumAverageArray.java
@@ -2,15 +2,11 @@ public class SumAverageArray {
     public static void main(String[] args) {
         int[] arr = { 3, 8, 2, 9, 6};
         int sum = 0;
-
         for (int num : arr) {
             sum += num;
         }
-
         double average = (double) sum / arr.length;
-
         System.out.println("Sum: " + sum);
         System.out.println("Average: " + average);
     }
-    
 }
