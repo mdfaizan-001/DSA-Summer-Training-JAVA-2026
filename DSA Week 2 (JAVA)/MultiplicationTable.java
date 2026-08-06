059d32979ace98c06fff3647333457a0e2c27c83
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-06 19:40:24 +0530

    Format and clean up MultiplicationTable.java

--- DSA Week 2 (JAVA)/MultiplicationTable.java
+++ DSA Week 2 (JAVA)/MultiplicationTable.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-
 public class MultiplicationTable {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter Number: ");
             int num = sc.nextInt();
-
             for(int i = 1; i <= 10; i++) {
                 System.out.println(num + " x " + i + " = " + (num * i));
             }
