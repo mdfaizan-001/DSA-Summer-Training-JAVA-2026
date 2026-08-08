b3513fcf35c0c3d856c1b84466d50675d49e3498
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-08 22:57:10 +0530

    Update Overloading.java

--- DSA Week 3 (JAVA)/Overloading.java
+++ DSA Week 3 (JAVA)/Overloading.java
@@ -1,20 +1,15 @@
 class Helper {
-
     // Method 1
     static int Multiply(int a, int b) {
         return a * b;
     }
-
     // Method 2 (Overloaded)
     static double Multiply(double a, double b) {
         return a * b;
     }
 }
-
 public class Overloading {
-
     public static void main(String[] args) {
-
         System.out.println(Helper.Multiply(2, 4));
         System.out.println(Helper.Multiply(5.5, 6.3));
     }
