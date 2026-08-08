a34928cb463a6cfaabad856f53e6ba8ad7bd5251
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-08 22:58:49 +0530

    Refactor Premove.java for cleaner syntax

--- DSA Week 4 (JAVA)/Premove.java
+++ DSA Week 4 (JAVA)/Premove.java
@@ -1,15 +1,11 @@
 import java.util.PriorityQueue;
 import java.util.Queue;
-
 public class Premove {
-
     public static void main(String args[]) {
-
         Queue<String> pq = new PriorityQueue<>();
         pq.add("Life");
         pq.add("is");
         pq.add("Beautiful");
-
         System.out.println("Initial Queue: " + pq);
         pq.remove("Good");
         System.out.println("After Remove: " + pq);
