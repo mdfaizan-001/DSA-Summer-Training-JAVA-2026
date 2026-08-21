71f1372685b90c4b06988c8cdc54821313e96cd5
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-21 20:15:03 +0530

    Refactor ArraySearch for cleaner code

--- DSA Week 1 (JAVA)/ArraySearch.java
+++ DSA Week 1 (JAVA)/ArraySearch.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class ArraySearch {
-    
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int[] arr = new int[5];
@@ -10,7 +9,6 @@ public class ArraySearch {
             for(int i = 0; i < 5; i++) {
                 arr[i] = sc.nextInt();
             }
-            
             System.out.print("Enter element to search: ");
             search = sc.nextInt();
             for(int i = 0; i < 5; i++) {
@@ -19,7 +17,6 @@ public class ArraySearch {
                     break;
                 }
             }
-            
             if(found)
                 System.out.println("Element Found");
             else
