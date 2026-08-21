c577caa728264731f77a6f5ac308ea970dab091c
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-21 20:12:58 +0530

    Update ArrayElementCount.java

--- DSA Week 1 (JAVA)/ArrayElementCount.java
+++ DSA Week 1 (JAVA)/ArrayElementCount.java
@@ -1,5 +1,7 @@
 public class ArrayElementCount {
+    
     public static void main(String[] args) {
+        
         int[] arr = {10, 20, 30, 40, 50};
         int count = arr.length;
         System.out.println("Total number of elements = " + count);
