7751f58f8b7ab4ea77c4802d236b0c1911780c5f
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-01 20:14:34 +0530

    Format code in ArrayStoreDisplay.java

--- DSA Week 1 (JAVA)/ArrayStoreDisplay.java
+++ DSA Week 1 (JAVA)/ArrayStoreDisplay.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArrayStoreDisplay {
+public class ArrayStoreDisplay {    
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int[] arr = new int[5];
