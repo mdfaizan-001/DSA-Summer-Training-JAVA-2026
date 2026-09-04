61a810befb3298ccac0cc4489dfd5cc3b422e431
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-04 21:11:47 +0530

    Update Unicodevalue.java

--- DSA Week 4 (JAVA)/Unicodevalue.java
+++ DSA Week 4 (JAVA)/Unicodevalue.java
@@ -1,23 +1,29 @@
 public class Unicodevalue {
     public static void main(String[] args) {
+
         System.out.println((int)'A');
         System.out.println((int)'a');
         System.out.println((int)'B');
         System.out.println((int)'b');
         System.out.println((int)'C');
+        
         System.out.println((int)'c');
         System.out.println((int)'D');
+        
         System.out.println((int)'d');
         System.out.println((int)'E');
         System.out.println((int)'e');
+        
         System.out.println((int)'F');
         System.out.println((int)'f');
         System.out.println((int)'G');
         System.out.println((int)'g');
+        
         System.out.println((int)'A');
         System.out.println((int)'a');
         System.out.println((int)'B');
         System.out.println((int)'b');
+        
         System.out.println((int)'C');
         System.out.println((int)'c');
         System.out.println((int)'D');
