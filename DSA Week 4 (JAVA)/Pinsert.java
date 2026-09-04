b6ad6d62ccbef257aa40657e774d0b41886e7ca1
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-04 21:09:46 +0530

    Format code in Pinsert.java for better readability

--- DSA Week 4 (JAVA)/Pinsert.java
+++ DSA Week 4 (JAVA)/Pinsert.java
@@ -2,10 +2,14 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 public class Pinsert {
     public static void main(String[] args) {
+        
         Queue<String> pq = new PriorityQueue<>();
         pq.add("Life");
+        
         pq.add("is");
+        
         pq.add("Beautiful");
+        
         System.out.println(pq);
     }
 }
