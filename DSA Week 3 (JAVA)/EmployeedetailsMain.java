d4ce0617f445b7e19e1926ab404a2d85ed322270
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-09 22:11:57 +0530

    Update EmployeedetailsMain.java

--- DSA Week 3 (JAVA)/EmployeedetailsMain.java
+++ DSA Week 3 (JAVA)/EmployeedetailsMain.java
@@ -1,5 +1,4 @@
-class Employee {
-    
+class Employee { 
     String empName;
     int empId;
     String phoneNo;
@@ -9,14 +8,12 @@ class Employee {
         System.out.println("Phone No: " + phoneNo);
     }
 }
-
 public class EmployeedetailsMain {
     public static void main(String[] args) {
         Employee e1 = new Employee();
         e1.empName = "Faizan";
         e1.empId = 101;
         e1.phoneNo = "9876543210";
-        e1.display();
-        
+        e1.display();   
     }
 }
