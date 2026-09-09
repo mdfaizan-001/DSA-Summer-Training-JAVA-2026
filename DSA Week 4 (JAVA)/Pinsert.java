4079ecb306ac8f3ab3067ca188b30fb0a17120aa
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-09 21:41:30 +0530

    Fix formatting issues in Pinsert.java

--- DSA Week 4 (JAVA)/Pinsert.java
+++ DSA Week 4 (JAVA)/Pinsert.java
@@ -1,8 +1,10 @@
 import java.util.PriorityQueue;
 import java.util.Queue;
 public class Pinsert {
-    public static void main(String[] args) {     
+    public static void main(String[] args) {   
+        
         Queue<String> pq = new PriorityQueue<>();
+        
         pq.add("Life");
         pq.add("is");
         pq.add("Beautiful");
